ca2cd5196fe5e403170a782a0862a87b9b1b3277
medlnb <mohamedlanabi0@gmail.com>
2025-02-07 23:04:26 +0100

    Fix: vervel bugs

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroCarousel from "./HeroCarousel";
-import HeroFeature from "./HeroFeature";
 import Image from "next/image";
 import heroBg from "@public/hero/hero-bg.png";
 import Link from "next/link";
@@ -39,7 +38,10 @@ const Hero = async () => {
       <div className="xl:max-w-[393px] w-full">
         <div className="flex flex-col sm:flex-row xl:flex-col h-full gap-5">
           {products.slice(2, 4).map((product) => (
-            <div className="w-full relative rounded-[10px] bg-white p-4 sm:p-7 flex-1">
+            <div
+              key={product._id}
+              className="w-full relative rounded-[10px] bg-white p-4 sm:p-7 flex-1"
+            >
               <div className="flex items-center justify-between">
                 <div>
                   <div className="max-w-[153px] font-semibold text-dark text-xl mb-13">
